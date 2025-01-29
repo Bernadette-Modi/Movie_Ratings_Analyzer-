90fa034882e43c2d59eab58200afcc2e8e972900
bernadette-modi <berniemain353gmail.com>
2025-01-29 21:54:00 +0300

    Prompted it to asked the user to input the movie name and proceed to rate it

--- movieratingsanalyzer.py
+++ movieratingsanalyzer.py
@@ -1,7 +1,18 @@
 #Movie Ratings Analyzer
 from array import *
-ratings = array.array('i', [3, 5, 4, 2, 5, 1, 4])
+movie = input("Enter the movie title: ")
+rate = (input("Enter movie ratings (1-5) separated by spaces: ")).split()
+
+ratings = array('i', [int(num) for num in rate])
 average_rating = sum(ratings) / len(ratings)
 highest_rating = max(ratings)
 lowest_rating = min(ratings)
-rating_counts = {i: rating_counts(i) for i in range(1,6)}
+rating_counts = {i: ratings.count(i) for i in range(1,6)}
+
+
+print(f"Average Rating: {average_rating:.2f}")
+print(f"Highest Rating: {highest_rating}")
+print(f"Lowest Rating: {lowest_rating}")
+print(f"Rating Counts for the movie {movie}: ")
+for rating, count in rating_counts.items():
+    print(f"⭐{rating} stars: {count} time(s)")
